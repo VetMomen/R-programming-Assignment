98d48507d8f13343e7e4a1fa67ed5cd226cf006b
VetMomen <vet.m.mohamed@gmail.com>
2018-10-22 00:35:54 +0200

    adding the fisrt assignment Function to specifiy the best hospital

--- Data Manipulation .R
+++ Data Manipulation .R
@@ -1,30 +1,49 @@
-outcome<-read_csv(file = 'D:/mo2men/R/data sets/Hospitals/outcome-of-care-measures.csv')
+outcome<-read_csv(file = 'D:/mo2men/R/data sets/Hospitals/outcome-of-care-measures.csv',
+                  col_types = 'ccccccccccncnnncncnnncncnnncncnnncncnnncncnnnc')
 
-#chanching class of some variables
-
-outcome<-outcome%>%mutate(`Hospital 30-Day Death (Mortality) Rates from Heart Attack`=as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Attack`),
-                          `Hospital 30-Day Death (Mortality) Rates from Heart Failure`=as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Failure`),
-                          `Hospital 30-Day Death (Mortality) Rates from Pneumonia`=as.numeric(`Hospital 30-Day Death (Mortality) Rates from Pneumonia`))
-
-
-#select the most important variables
-
-outcome_MR<-outcome%>%
-        select(`Hospital Name`,State,
-               `Hospital 30-Day Death (Mortality) Rates from Heart Attack`,
-               `Hospital 30-Day Death (Mortality) Rates from Heart Failure`,
-               `Hospital 30-Day Death (Mortality) Rates from Pneumonia`)%>%drop_na()%>%
-        data.frame()
-
+str(outcome)
+dim(outcome)
 #renaming variables 
-outcome_MR<-outcome_MR%>%rename('Heart Attack'=Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,
-                                'Hear Failure'=Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,
-                                'Pneumonia'=Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+names(outcome)<-c('code','Hospital_name',paste('Adress',c(1,2,3)),
+                  'city','state','zib_code','country_name','phone_number',
+                  paste('Heart_Attack',c('rate','comp','lower','upper','count','footnote'),sep = '_'),
+                  paste('Heart_Failure',c('rate','comp','lower','upper','count','footnote'),sep = '_'),
+                  paste('Pneumonia',c('rate','comp','lower','upper','count','footnote'),sep = '_'),
+                  paste('RHeart_Attack',c('rate','comp','lower','upper','count','footnote'),sep = '_'),
+                  paste('RHeart_Failure',c('rate','comp','lower','upper','count','footnote'),sep = '_'),
+                  paste('RPneumonia',c('rate','comp','lower','upper','count','footnote'),sep = '_'))
+
 
 #ploting heart attack mortality rate 
 
-outcome_MR%>%ggplot(aes(x=`Heart Attack`))+
-        geom_histogram()
+outcome%>%ggplot(aes(x=Heart_Attack_rate))+
+        geom_histogram(colour='black',fill='white')+
+        theme(panel.background = element_rect(fill = 'white'))+
+        xlab('Heart Attack Mortality Rate')+
+        ylab("")+
+        ggtitle('Frequency of heart attack mortality rate')
+        
 
 # building a function which choose the best hosbital 
 
+best<-function(df,state,outcome){
+        sat<-unique(df$state)
+        out<-c('Heart Attack','Heart Failure','Pneumonia')
+        if(any(state==sat)){
+                data1<-df[df$state==state,]
+                data2<-data1%>%select("Hospital_name","state","Heart_Attack_rate","Heart_Failure_rate",
+                                      "Pneumonia_rate")%>%drop_na()
+                if(outcome=='Heart Attack'){
+                        data4<-data2[data2$Heart_Attack_rate==min(data2$Heart_Attack_rate),"Hospital_name"]
+                } else if(outcome=='Heart Failure'){
+                        data4<-data2[data2$Heart_Failure_rate==min(data2$Heart_Failure_rate),"Hospital_name"]
+                }else if(outcome=='Pneumonia'){
+                        data4<-data2[data2$Pneumonia_rate==min(data2$Pneumonia_rate),"Hospital_name"]   
+                }else stop('invalide outcome')
+        } else {
+                stop('invalid state')
+        }
+        data4<-data4%>%arrange()
+        print(data4)
+}
+best(df = outcome,state = 'NY',outcome = 'Pneumonia')
