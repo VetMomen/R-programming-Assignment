108f9bac8ff7d1ec3fafb7d501fd818121169c7f
VetMomen <vet.m.mohamed@gmail.com>
2018-10-23 23:48:33 +0200

    adding the third assignment  #rankall function

--- Data Manipulation .R
+++ Data Manipulation .R
@@ -120,3 +120,23 @@ rankhospital<-function(df,state,outcome,ranke=1){
 rankhospital(df=outcome,state = 'AR',outcome = 'Heart Failure',ranke = 5)
 
 
+# adding function to rank all according to states
+
+rankall<-function(df,outcome,rank=1){
+        if(outcome=='Heart Attack'){
+                data1<-df%>%select(state,Hospital_name,Heart_Attack_rate,Heart_Failure_rate,Pneumonia_rate)
+                data2<-data1%>%group_by(state)%>%mutate('rank'=rank(Heart_Attack_rate,na.last = 'keep',ties.method = 'min'))%>%drop_na()
+                data3<-data2[data2$rank==rank,c('state','Hospital_name','rank')]
+        }else if(outcome=='Heart Failure'){
+                data1<-df%>%select(state,Hospital_name,Heart_Attack_rate,Heart_Failure_rate,Pneumonia_rate)
+                data2<-data1%>%group_by(state)%>%mutate('rank'=rank(Heart_Failure_rate,na.last = 'keep',ties.method = 'min'))%>%drop_na()
+                data3<-data2[data2$rank==rank,c('state','Hospital_name','rank')]
+        }else if(outcome=='Pneumonia'){
+                data1<-df%>%select(state,Hospital_name,Heart_Attack_rate,Heart_Failure_rate,Pneumonia_rate)
+                data2<-data1%>%group_by(state)%>%mutate('rank'=rank(Pneumonia_rate,na.last = 'keep',ties.method = 'min'))%>%drop_na()
+                data3<-data2[data2$rank==rank,c('state','Hospital_name','rank')]
+        }else{
+                stop('Invalid Outcome')
+        }
+        print(data3)
+}
